d9d9695877237e5f5eafb6fe2154978b77967186
Sam Falkner <sam.falkner@gmail.com>
2022-12-30 07:07:19 -0700

    empty session request object

--- authn.go
+++ authn.go
@@ -16,3 +16,9 @@ type SessionNotFound struct {
 	ID      string `json:"id"`
 	Message string `json:"message"`
 }
+
+// EmptySessionRequest is sent when requesting a new empty session cookie. The
+// CookieOK field must be true, or no cookie will be created.
+type EmptySessionRequest struct {
+	CookieOK bool `json:"cookie_ok"`
+}
