880b5ac348431640aae7f9af42671890feeffdc7
Sam Falkner <sam.falkner@gmail.com>
2023-01-03 06:48:32 -0700

    cleaning up the authx interface a bit

--- authn.go
+++ authn.go
@@ -1,24 +1,22 @@
 package api2
 
-// AuthInput is for authenticating with an efmrl
-type AuthInput struct {
+// AuthReq is for authenticating with an efmrl
+type AuthReq struct {
 	Value string `json:"value"`
 }
 
-// NoSessionCookie is given when there is no session cookie, or when the session
-// cookie is corrupt
-type NoSessionCookie struct {
+// AuthRes is returned after an authentication attempt
+type AuthRes struct {
 	Message string `json:"message"`
 }
 
-// SessionNotFound is given when a session ID is found but the session is not
-type SessionNotFound struct {
-	ID      string `json:"id"`
-	Message string `json:"message"`
-}
-
-// EmptySessionRequest is sent when requesting a new empty session cookie. The
+// EmptySessionReq is sent when requesting a new empty session cookie. The
 // CookieOK field must be true, or no cookie will be created.
-type EmptySessionRequest struct {
+type EmptySessionReq struct {
 	CookieOK bool `json:"cookie_ok"`
+}
+
+// EmptySessionErr is returned with the request
+type EmptySessionErr struct {
+	Message string `json:"message"`
 }
