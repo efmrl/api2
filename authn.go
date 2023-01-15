e5ecbf21bfa94bfdb09215cd3f534a173be52950
Sam Falkner <sam.falkner@gmail.com>
2023-01-15 15:53:56 -0700

    make some things more regular

--- authn.go
+++ authn.go
@@ -1,22 +1,13 @@
 package api2
 
-// AuthReq is for authenticating with an efmrl
-type AuthReq struct {
-	Value string `json:"value"`
-}
-
-// AuthRes is returned after an authentication attempt
-type AuthRes struct {
-	Message string `json:"message"`
-}
-
-// EmptySessionReq is sent when requesting a new empty session cookie. The
+// SessionReq is sent when requesting a new empty session cookie. The
 // CookieOK field must be true, or no cookie will be created.
-type EmptySessionReq struct {
+type SessionReq struct {
 	CookieOK bool `json:"cookie_ok"`
 }
 
-// EmptySessionErr is returned with the request
-type EmptySessionErr struct {
-	Message string `json:"message"`
+// SessionRes is returned with the request
+type SessionRes struct {
+	Message string `json:"message,omitempty"`
+	Created string `json:"created,omitempty"`
 }
