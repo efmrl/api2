451ab3ce7d12a9d4c70653ce3839a2bd041bf282
Sam Falkner <sam.falkner@gmail.com>
2023-08-07 06:21:43 -0600

    moving some constants into api2

---
+++ const.go
@@ -0,0 +1,6 @@
+package api2
+
+const (
+	// DefaultAPIPrefix is used when an efmrl doesn't have an APIPrefix set
+	DefaultAPIPrefix = "/.e"
+)
---
+++ cookie.go
@@ -0,0 +1,8 @@
+package api2
+
+const (
+	// StrictCookieName is the name of our strict cookie
+	StrictCookieName = "__Host-E"
+	// SessionCookieName is the name of our session cookie
+	SessionCookieName = "__Secure-E"
+)
