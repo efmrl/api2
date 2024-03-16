a817fd21afe6fc3e0eb958462ed94dac2be1f351
Sam Falkner <sam.falkner@gmail.com>
2024-03-16 15:41:28 -0600

    efmrl name operations

--- names.go
+++ names.go
@@ -1,5 +1,19 @@
 package api2
 
+type PostNameReq struct {
+	Name string `json:"name,omitempty"`
+}
+
+type PostNameRes struct {
+	Name string `json:"name"`
+}
+
 type GetNamesRes struct {
-	Names []string `json:"names"`
+	Names []*EfmrlName `json:"names"`
+}
+
+type EfmrlName struct {
+	Name      string `json:"name"`
+	Link      string `json:"link,omitempty"`
+	DeleteURL string `json:"deleteURL,omitempty"`
 }
