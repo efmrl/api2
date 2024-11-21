54ccebacab05523f7e81f856bb5384e0962cba42
Sam Falkner <sam.falkner@gmail.com>
2024-11-21 09:06:20 -0700

    a login token getting API

--- session.go
+++ session.go
@@ -84,3 +84,11 @@ type UserLogin struct {
 	When     string `json:"when"`
 	Friendly string `json:"friendly"`
 }
+
+// GetTokenRes returns a login token
+type GetTokenRes struct {
+	// Token is the login token
+	Token string `json:"token"`
+	// Expires is when the token expires, in RFC 3339 format.
+	Expires string `json:"created,omitempty"`
+}
