8319ce013c64d7d85ca5a17fa9b1b3ad953807bc
Sam Falkner <sam.falkner@gmail.com>
2025-02-27 06:22:19 -0700

    improvements found when implementing admin of mounts

--- mount.go
+++ mount.go
@@ -2,6 +2,8 @@ package api2
 
 // Mount holds an md.Mount for use in the http server
 type Mount struct {
+	ID         string                 `json:"id"`
+	Path       string                 `json:"path"`
 	FileSystem string                 `json:"filesystem,omitempty"`
 	Specials   *SpecialPerms          `json:"specials,omitempty"`
 	Princs     map[string]*MountPerms `json:"princs,omitempty"`
@@ -10,28 +12,16 @@ type Mount struct {
 // MountPerms holds permissions for a princ with respect to a mount
 type MountPerms struct {
 	Perms     Perm  `json:"perms"`
-	FileQuota int32 `json:"file_quota"`
-	ByteQuota int32 `json:"byte_quota"`
+	FileQuota int32 `json:"fileQuota"`
+	ByteQuota int32 `json:"byteQuota"`
 }
 
 // GetMountsRes is the result of getting an efmrl's mounts
 type GetMountsRes struct {
 	// Mounts is the list of all mounts on the efmrl
-	Mounts map[string]*Mount `json:"mounts"`
+	Mounts []*Mount `json:"mounts"`
 }
 
-// PostMountReq is a request to create a mount
-type PostMountReq struct {
-	Path  string `json:"path"`
-	Mount Mount  `json:"mount"`
-}
-
-// PostMountRes is the result of posting a mount
 type PostMountRes struct {
-	Message string `json:"message"`
-}
-
-// DeleteMountRes is the result of deleting a mount
-type DeleteMountRes struct {
-	Message string `json:"message"`
+	ID string `json:"id"`
 }
